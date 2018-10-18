e46d80cd1f1fd697847b703fe537f59b453b72e3
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2018-10-18 18:20:34 +0100

    add pagination to list of courses

--- src/components/Courses/index.jsx
+++ src/components/Courses/index.jsx
@@ -1,12 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
+import Pagination from 'react-js-pagination';
 
 import * as actions from '../../actions/courses';
 import CoursesList from './CourseList';
 
 class Courses extends Component {
+  itemsCountPerPage = 3
+
+  state = {
+    activePage: 1,
+  }
+
   redirectToAddCoursePage = () => {
     const { history } = this.props;
     history.push('/courses/create');
@@ -17,8 +24,21 @@ class Courses extends Component {
     deleteCourse(id);
   }
 
+  handlePageChange = (pageNumber) => {
+    this.setState({ activePage: pageNumber });
+  }
+
+  itemsSliceForCurrentPage = () => {
+    const { activePage } = this.state;
+
+    const start = (activePage - 1) * this.itemsCountPerPage;
+    const end = start + this.itemsCountPerPage;
+    return [start, end];
+  }
+
   render() {
     const { courses } = this.props;
+    const { activePage } = this.state;
 
     return (
       <div>
@@ -33,7 +53,23 @@ class Courses extends Component {
         {
           courses.length === 0
             ? <p className="font-weight-bold">There are currently no courses available</p>
-            : <CoursesList courses={courses} deleteCourse={this.deleteCourse} />
+            : (
+              <Fragment>
+                <CoursesList
+                  courses={courses.slice(...this.itemsSliceForCurrentPage())}
+                  deleteCourse={this.deleteCourse}
+                />
+                <Pagination
+                  itemsCountPerPage={this.itemsCountPerPage}
+                  totalItemsCount={courses.length}
+                  pageRangeDisplayed={10}
+                  onChange={this.handlePageChange}
+                  itemClass="page-item"
+                  linkClass="page-link"
+                  activePage={activePage}
+                />
+              </Fragment>
+            )
         }
       </div>
     );
