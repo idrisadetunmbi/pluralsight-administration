0adda3a4c1794458728ba8477c38af839b53686c
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2018-10-18 22:18:43 +0100

    improve redux setup configuration

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,8 +3,13 @@ import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
 import reducers from '../reducers';
 
+const middleware = [thunk];
+if (process.env.NODE_ENV === 'developemnt') {
+  middleware.push(reduxImmutableStateInvariant());
+}
+
 export default initialState => createStore(
   reducers,
   initialState,
-  applyMiddleware(thunk, reduxImmutableStateInvariant()),
+  applyMiddleware(...middleware),
 );
